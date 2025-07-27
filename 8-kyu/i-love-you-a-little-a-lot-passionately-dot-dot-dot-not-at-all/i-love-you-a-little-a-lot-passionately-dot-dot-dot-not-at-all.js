c6c9f6b767812413aa3045cac06ec436f897847e
Gisaro Billy <gisaroblanks@gmail.com>
2025-07-27 15:31:04 +0200

    Update solution - CodeHub

--- 8-kyu/i-love-you-a-little-a-lot-passionately-dot-dot-dot-not-at-all/i-love-you-a-little-a-lot-passionately-dot-dot-dot-not-at-all.js
+++ 8-kyu/i-love-you-a-little-a-lot-passionately-dot-dot-dot-not-at-all/i-love-you-a-little-a-lot-passionately-dot-dot-dot-not-at-all.js
@@ -1,5 +1,5 @@
 function howMuchILoveYou(nbPetals) {
-const pharses = ["I love you","a little","a lot","passionately","madly","not at all"];
-  return pharses [(nbPetals - 1) % 6];
+  return ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all'][(nbPetals - 1) % 6];
 }
+​
 ​
