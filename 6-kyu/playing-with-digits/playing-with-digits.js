127e2271119cd24187420991e3b3948516a1d4fa
Gisaro Billy <gisaroblanks@gmail.com>
2025-07-29 15:33:00 +0200

    Update solution - CodeHub

--- 6-kyu/playing-with-digits/playing-with-digits.js
+++ 6-kyu/playing-with-digits/playing-with-digits.js
@@ -1,9 +1,4 @@
 function digPow(n, p){
-const digits = String(n).split('').map(Number);
-  let sum = 0;
-  for( let i = 0; i < digits.length ; i++){
-    sum += Math.pow(digits[i],p + i);
-  }
-  const k = sum/n;
-  return Number.isInteger(k) && k > 0 ? k : -1;
+  const result = `${n}`.split('').reduce((sum,i) => sum += i ** p++,0)
+  return result % n ? -1:result/n
 }
