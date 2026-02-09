c5c018f48d58a5eb7d7f10cf6226ed5c131563fd
Gisaro Billy <gisaroblanks@gmail.com>
2026-02-09 10:55:43 +0200

    Add solution - CodeHub

---
+++ 6-kyu/who-likes-it/who-likes-it.py
@@ -0,0 +1,14 @@
+def likes(names):
+    count = len(names)
+    
+    if count == 0:
+        return "no one likes this"
+    elif count == 1:
+        return f"{names[0]} likes this"
+    elif count == 2:
+        return f"{names[0]} and {names[1]} like this"
+    elif count == 3:
+        return f"{names[0]}, {names[1]} and {names[2]} like this"
+    else:
+        return f"{names[0]}, {names[1]} and {count - 2} others like this"
+​
