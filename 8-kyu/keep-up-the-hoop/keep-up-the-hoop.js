cd22aa2a2689b3fba2366975c61075577f0846a4
Billy Gisaro <gisaroblanks@gmail.com>
2026-03-30 10:22:56 +0200

    Update solution - CodeHub

--- 8-kyu/keep-up-the-hoop/keep-up-the-hoop.js
+++ 8-kyu/keep-up-the-hoop/keep-up-the-hoop.js
@@ -1,8 +1,7 @@
 function hoopCount (n) {
-   //your code goes here    
-  if (n >= 10){
-    return "Great, now move on to tricks";
-  } else{
-    return "Keep at it until you get it";
-  }
+   if(n >= 10){
+     return "Great, now move on to tricks"
+   } else{
+     return "Keep at it until you get it"
+   }  
 }
