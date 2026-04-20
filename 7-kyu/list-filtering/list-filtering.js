31fa2f763b5e4ad20ae326daa32fdec664223b1a
Billy Gisaro <gisaroblanks@gmail.com>
2026-04-20 09:56:19 +0200

    Update solution - CodeHub

--- 7-kyu/list-filtering/list-filtering.js
+++ 7-kyu/list-filtering/list-filtering.js
@@ -1,5 +1,5 @@
 function filter_list(l) {
-  // Return a new array with the strings filtered out
-   return l.filter(function(n){return typeof n == 'number'});
+return l.filter(n => {
+  return typeof n == 'number'
+})
 }
-​
