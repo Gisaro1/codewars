3eef5130f4403225ff77940527a9d1e008e732a6
Billy Gisaro <gisaroblanks@gmail.com>
2026-04-22 13:49:46 +0530

    Update solution - CodeHub

--- 7-kyu/get-the-middle-character/get-the-middle-character.js
+++ 7-kyu/get-the-middle-character/get-the-middle-character.js
@@ -1,10 +1,9 @@
 function getMiddle(s) {
-  //Code goes here!
-   let abt = s.length;
-  let middle = Math.floor(abt / 2)
-  if (abt % 2 === 0){
-    return s.substring(middle -1, middle +1);
-  } else {
-    return s.substring(middle,middle +1);
+  let length = s.length
+  let average = Math.floor(length /2)
+  if(length % 2 === 0){
+    return s.substring(average-1,average+1)
+  } else{
+    return s.substring(average,average+1)
   }
 }
