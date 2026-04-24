77853fd600974635c3b61eaa5d3674e5b97e99f9
Billy Gisaro <gisaroblanks@gmail.com>
2026-04-24 13:42:35 +0530

    Update solution - CodeHub

--- 7-kyu/find-the-next-perfect-square/find-the-next-perfect-square.js
+++ 7-kyu/find-the-next-perfect-square/find-the-next-perfect-square.js
@@ -1,9 +1,8 @@
 function findNextSquare(sq) {
-const root = Math.sqrt(sq)
-if(Number.isInteger(root)){
-  const nextRoot = root + 1;
-  return nextRoot * nextRoot
-} else{
-  return -1;
-}
+    const root = Math.sqrt(sq);
+    if (root % 1 === 0) {          // root is integer if no decimal remainder
+        const nextRoot = root + 1;
+        return nextRoot * nextRoot;
+    }
+    return -1;
 }
