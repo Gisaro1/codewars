c71a666bda8ef71968fafe4faa127946883439fe
Billy Gisaro <gisaroblanks@gmail.com>
2026-04-29 13:24:46 +0530

    Update solution - CodeHub

--- 7-kyu/sum-of-two-lowest-positive-integers/sum-of-two-lowest-positive-integers.js
+++ 7-kyu/sum-of-two-lowest-positive-integers/sum-of-two-lowest-positive-integers.js
@@ -1,5 +1,6 @@
 function sumTwoSmallestNumbers(numbers) {  
   // Code here
-  const sorted = numbers.sort((a,b) => a-b);
-  return sorted[0] + sorted[1];
+ const sorted= numbers.sort((a,b) => a - b);
+  return sorted[0] + sorted[1]
 }
+​
