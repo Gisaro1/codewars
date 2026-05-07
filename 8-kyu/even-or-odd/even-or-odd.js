bd3bd8b49b6f71631c42e50bd1c93c10dc594d0e
Billy Gisaro <gisaroblanks@gmail.com>
2026-05-07 17:50:46 +0530

    Update solution - CodeHub

--- 8-kyu/even-or-odd/even-or-odd.js
+++ 8-kyu/even-or-odd/even-or-odd.js
@@ -1,7 +1,7 @@
 function evenOrOdd(number) {
- if (number % 2 !== 0){
-   return 'Odd';
- } else {
-   return 'Even';
- }
+  if(number % 2 === 0){
+    return 'Even'
+  } else{
+    return 'Odd'
+  }
 }
