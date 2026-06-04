fac2a8ecde2810fcce56a6ad09a99008f7612ae7
Gisaro1 <gisaroblanks@gmail.com>
2026-06-04 11:00:57 +0200

    changes

--- 6-kyu/who-likes-it/who-likes-it.js
+++ 6-kyu/who-likes-it/who-likes-it.js
@@ -1,8 +1,8 @@
-function likes(names) {
-    if (names.length === 0) return "no one likes this";
-    if (names.length === 1) return `${names[0]} likes this`;
-    if (names.length === 2) return `${names[0]} and ${names[1]} like this`;
-    if (names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
-}
+const likes = (names) => {
+     return (names.length === 0) ?"no one likes this": 
+     (names.length === 1) ? `${names[0]} likes this`:
+     (names.length === 2) ? `${names[0]} and ${names[1]} like this`: 
+     (names.length === 3) ?  `${names[0]}, ${names[1]} and ${names[2]} like this`:
+     `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+};
 console.log(likes(["Peter"]));
