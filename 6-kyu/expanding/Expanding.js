200328b11d09959ff02e9f0dd7ccb9be79637e33
Gisaro1 <gisaroblanks@gmail.com>
2026-06-28 09:56:19 +0200

    feat:used repeat

---
+++ 6-kyu/expanding/Expanding.js
@@ -0,0 +1,13 @@
+const expandedForm = num => {
+const str = num.toString();
+const result = [];
+for(let i = 0;i < str.length;i++){
+    let char = str[i];
+    if(char !== '0'){
+        const zero = '0'.repeat(str.length - 1- i);
+        result.push(char + zero)
+    }
+}
+return result.join(' + ')
+}
+console.log(expandedForm(22))
