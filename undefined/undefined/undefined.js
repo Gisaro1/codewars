ffcf316936c3e3730f6ca85581a596ed3d885765
Gisaro1 <gisaroblanks@gmail.com>
2026-06-30 12:30:12 +0200

    feat:missin Letter

--- undefined/undefined/undefined.js
+++ undefined/undefined/undefined.js
@@ -11,3 +11,18 @@ const missing = arr=>{
     return missin
 }
 console.log(missing([0,2,3,4,8]))
+
+const missinLetter = arr =>{
+    const sett = new Set(arr);
+const start = arr[0].charCodeAt(0);
+const end = arr[arr.length -1].charCodeAt(0);
+const missinn = []
+for(let i = start; i<= end;i++){
+    const letter = String.fromCharCode(i)
+    if(!sett.has(letter)){
+        missinn.push(letter);
+    }
+}
+return missinn;
+}
+console.log(missinLetter(['a','c','d','g']))
