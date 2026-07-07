b2650abef67bfcb8bfb9465415e8cbfc7db4b566
Gisaro1 <gisaroblanks@gmail.com>
2026-07-07 12:51:15 +0200

    feat:used easy way

--- 8-kyu/Beginner Series #1 School Paperwork/Beginner Series #1 School Paperwork.js
+++ 8-kyu/Beginner Series #1 School Paperwork/Beginner Series #1 School Paperwork.js
@@ -1,4 +1,2 @@
-const paperwork = (n, m) => {
- return (n < 0 || m < 0) ? 0 : m * n;
-}
-console.log(paperwork(5,-5))
+const paperwork = (n, m) =>(n < 0 || m < 0) ? 0 : m * n;
+console.log(paperwork(5,2))
