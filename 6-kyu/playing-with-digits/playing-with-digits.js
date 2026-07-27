ee8b7ef1ea9d62726df0687d0ee5b350620bc6d7
Gisaro1 <gisaroblanks@gmail.com>
2026-07-27 16:46:37 +0200

    playing with digits

--- 6-kyu/playing-with-digits/playing-with-digits.js
+++ 6-kyu/playing-with-digits/playing-with-digits.js
@@ -1,4 +1,15 @@
-function digPow(n, p){
-  const result = `${n}`.split('').reduce((sum,i) => sum += i ** p++,0)
-  return result % n ? -1:result/n
+const digPow = (n,p) =>{
+  let total = 0
+  const splitN = String(n).split('')
+  for(let i = 0; i < splitN.length;i++){
+    let char = splitN[i];
+    const mult = Math.pow(parseInt(char,),p++)
+    total += mult;
+  }
+  if(total % n === 0){
+    return total/n
+  } else{
+    return -1
+  }
 }
+console.log(digPow(46288,3))
