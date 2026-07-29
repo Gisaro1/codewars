615f85f14e098859333faf59414c250299d3a581
Gisaro1 <gisaroblanks@gmail.com>
2026-07-29 18:21:00 +0200

    unique text

---
+++ 5-kyu/calculating-with-functions/Find the unique string/Find the unique string.js
@@ -0,0 +1,11 @@
+const findUniq = arr =>{
+    let text = arr.find(char =>{
+       let lower =  char.toLowerCase();
+       let other = arr.find(txt => !txt.toLowerCase().includes(lower[0]));
+       if(other){
+        return arr.filter(chr => chr.toLowerCase().includes(lower[0])).length === 1;
+       }
+    })
+    return text
+}
+console.log(findUniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ]))
