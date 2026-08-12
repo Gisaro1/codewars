5e4b1d5235ef5784f1288d94061423aa754117fd
Billy Gisaro <gisaroblanks@gmail.com>
2026-08-12 10:02:42 +0000

    reduce version2

--- 7-kyu/Number/number.js
+++ 7-kyu/Number/number.js
@@ -1,4 +1,4 @@
 const number = busStops => {
-  return busStops.reduce((total, [enter,out]) => total + (enter - out, 0));
+  return busStops.reduce((total, [enter,out]) => total + (enter - out), 0);
 }
 console.log(number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]))
