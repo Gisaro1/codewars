a05c38ea5bd64e0190107c6ca9d790a5960dd3d9
Gisaro1 <gisaroblanks@gmail.com>
2026-08-27 10:04:35 +0200

    remaining final touches

--- 6-kyu/SquareDigitsSequence/Square Digits Sequence.js
+++ 6-kyu/SquareDigitsSequence/Square Digits Sequence.js
@@ -1,9 +1,14 @@
 const squareDigitsSequence = ab => {
     let result = [];
-    const change =  String(ab).split('').map(Number);
-    const sqr = change.map(char => Math.pow(char,2)).reduce((total,num) => total + num, 0);
-    result.push(sqr)
-    return result
+
+    let current = ab
+    while(String(current).length > 1){
+        const change =  String(current).split('').map(Number);
+        const sqr = change.map(char => Math.pow(char,2)).reduce((total,num) => total + num, 0);
+        result.push(sqr)
+        current = sqr
+    }
+    return result.length
 
 }
 console.log(squareDigitsSequence(16))
