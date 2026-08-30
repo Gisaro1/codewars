c4b64cf091d2a86135475ee8d2ba49c9d69c2cf7
Gisaro1 <gisaroblanks@gmail.com>
2026-08-30 09:31:20 +0200

    checked if add works well

--- Objects/Expressive Objects/Expressive Objects.js
+++ Objects/Expressive Objects/Expressive Objects.js
@@ -1,5 +1,17 @@
-const evaluate = (left, op, right) => {
+const evaluate = (left) => {
     for(const key in left['multiply']){
-        console.log(key)
+        if(key === 'add'){
+            const sum = left['multiply']['add'].reduce((num,acc) => acc + num, 0)
+            return (`add: ${sum}`)
+        }
     }
 }
+const left = {
+    'multiply': {
+        'add': [1, 2, 3, 4, 5],
+        'subtract': [5, 6, 7, 8, 9],
+        'multiply': [1, 2, 3, 4, 5],
+        'divide': [1, 2, 3, 4, 5]
+    }
+};
+console.log(evaluate(left))
