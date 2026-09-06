0df34978591e5c523254fcb12480dde159e769f6
Gisaro1 <gisaroblanks@gmail.com>
2026-09-06 21:55:22 +0200

    my way

---
+++ 7-kyu/Count the divisors of a number/Count the divisors of a number.js
@@ -0,0 +1,11 @@
+const getDivisorsCnt = n => {
+    let count = 0;
+    for(let i = 0; i <= n; i++){
+        if(n % i === 0){
+            count++
+        } else {
+            return;
+        }
+        return count
+    }
+}
