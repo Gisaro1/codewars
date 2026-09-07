84282c0483bca6518db66a2efab7ee5efad9b399
Gisaro1 <gisaroblanks@gmail.com>
2026-09-07 13:41:44 +0200

    solved it

--- Promises/PromiseOptimizer/replace/example.js
+++ Promises/PromiseOptimizer/replace/example.js
@@ -1,7 +1,14 @@
 const splittin = num => {
+    let str = num.toString()
     let result = [];
-    let split = num.split('')
-    for(let i = 0; i < split.length; i++){
-        
+    for(let i = 0 ;i < str.length; i++){
+        let char = str[i]
+        if(char !== '0'){
+            let number = Number(char)
+            let answer = number * Math.pow(10,str.length-1 - i)
+            result.push(answer)
+        }
     }
-};
+    return result.join(' + ')
+}
+console.log(splittin(12))
