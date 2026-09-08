79560ac4ac82393583b95e4154df8667cc252ca1
Gisaro1 <gisaroblanks@gmail.com>
2026-09-08 19:45:04 +0200

    used promise way to solve limit with concurent by use of async functions

--- Promises/PromiseOptimizer/replace/concurency.js
+++ Promises/PromiseOptimizer/replace/concurency.js
@@ -1,38 +1,27 @@
-function limitAsync(tasks, limit) {
-  const result  = new Array(tasks.lenght)
-  let flight = 0;
-  let nextIndex = 0;
-  return new Promise((res,rej) => {
-    const helper = () => {
-      while(flight < limit && nextIndex < tasks.length){
-        const currentIndex = nextIndex
-        const task = tasks[currentIndex]
-        flight++;
-        nextIndex++;
-        fetch(task)
-            .then((res) => {
-                if(!res.ok) throw new Error(`Oops ${res.status}`)
-                    return res.json();
-            })
-            .then(data => {
-                result[currentIndex] = {status:"fullfilled", value:data}
-            })
-            .catch(err => {
-                result[currentIndex] = {status:"rejected", value:err}
-            })
-            .finally(() => {
-                flight--;
-                if(nextIndex < tasks.lenght){
-                    helper()
-                } else if(flight === 0){
-                    res(result)
-                }
-            })
-      }
+const limitAsync= async(tasks, limit) => {
+    let index = 0;
+    const result = new Array(tasks.length);
+    const helper = async() => {
+        while(index < tasks.length){
+            const currentIndex = index++;
+            const task = tasks[currentIndex];
+
+            try{
+                const response = await fetch(task);
+                if(!response.ok) throw new Error(`Oops something went wrong at: ${response.status}`)
+                    const data = await response.json()
+                    result[currentIndex] = {status:"fullfilled", value:data}
+            } catch(error){
+                result[currentIndex] = {status:"rejected", reason:error}
+            }
+        }
+    };
+    const worker = [];
+    for(let i = 0; i < limit;i++){
+        worker.push(helper());
     }
-    helper();
-    helper();
-  })
+    await Promise.all(worker)
+    return result
 }
 const urls = [
   "https://jsonplaceholder.typicode.com/todos/1",
@@ -41,4 +30,4 @@ const urls = [
   "https://jsonplaceholder.typicode.com/todos/4",
   "https://jsonplaceholder.typicode.com/todos/5"
 ];
-limitAsync(urls,3).then(console.log)
+console.log(await limitAsync(urls,2))
