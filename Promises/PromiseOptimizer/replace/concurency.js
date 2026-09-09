7dab2b62e8bdbc8317124c6c0f5f2bf9ea95bb23
Gisaro1 <gisaroblanks@gmail.com>
2026-09-09 20:53:42 +0200

    changed sample test

--- Promises/PromiseOptimizer/replace/concurency.js
+++ Promises/PromiseOptimizer/replace/concurency.js
@@ -25,7 +25,7 @@ const limitAsync= async(tasks, limit) => {
 }
 const urls = [
   "https://jsonplaceholder.typicode.com/todos/1",
-  "https://jsonplaceholder.typicode.com/todos/",
+  "https://jsonplaceholder.typicode.com/todos/2",
   "https://jsonplaceholder.typicode.com/todos/3",
   "https://jsonplaceholder.typicode.com/todos/4",
   "https://jsonplaceholder.typicode.com/todos/5"
