017e9ddd8f7b3287bf7d9cdbbbfb5c6170c35bab
Gisaro1 <gisaroblanks@gmail.com>
2026-09-11 11:17:38 +0200

    5kyu

---
+++ 5-kyu/calculating-with-functions/Calculating with Functions/Calculating with Functions.js
@@ -0,0 +1,36 @@
+const zero = operation => {
+  return operation ? operation(0): 0 
+}
+const one = operation => {
+  return operation ? operation(1): 1 
+}
+const two = operation => {
+  return operation ? operation(2): 2 
+}
+const three = operation => {
+  return operation ? operation(3): 3
+}
+const four = operation => {
+  return operation ? operation(4): 4 
+}
+const five = operation => {
+  return operation ? operation(5): 5
+}
+const six = operation => {
+  return operation ? operation(6): 6
+}
+const seven = operation => {
+  return operation ? operation(7): 7 
+}
+const eight = operation => {
+  return operation ? operation(8): 8
+}
+const nine = operation => {
+  return operation ? operation(9): 9 
+}
+
+const plus = b => a => a + b
+const minus = b => a => a - b   
+const times = b => a => a * b
+const dividedBy = b => a => a / b 
+console.log(seven(times(five())))
